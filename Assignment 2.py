3a3b96ecaa1786c91296e8e141a066669c1360c8
Sourav Verma <59683971+RajputVerma@users.noreply.github.com>
2025-09-22 11:42:12 +0530

    Add files via upload
    This is a Second Assignment of python for TuTeDude

---
+++ Assignment 2.py
@@ -0,0 +1,19 @@
+'''Write a python program that :
+Check if the number is even or odd'''
+#This is a program which shows us that the number is Even or Odd
+'''a=int(input("Enter a number : "))
+if a % 2 == 0:
+    print(a,"is an Even number. ")
+else :
+    print(a,"is an Odd number.")'''
+'''Task 2: Sum of Integers from 1 to 50 Using a Loop
+ 
+Problem Statement: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+'''
+sum = 0  #this is variable sum that store value of sum
+for num in range(1, 51):#in this statement num is a variable which has range from 1 to 50
+    sum += num   # add each number to the sum
+print("The sum of numbers from 1 to 50 is:", sum)#Here Print the sum
